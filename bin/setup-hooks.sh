5aefbd30c6ac125bf8389caea321c711f3a29694
Erin Grant <e.grant41@gmail.com>
2020-10-20 11:55:59 -0400

    setup hooks for uploading to Google Drive

--- bin/setup-hooks.sh
+++ bin/setup-hooks.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# Set up hooks for this TeX paper repo.
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-repodir=$(dirname pwd)
+repodir=$(dirname $scriptdir)
 hash=`echo -n $(git config --local remote.origin.url) | sha1sum | awk '{print $1}'`
+
+# Point to a Google Drive folder mounted with https://github.com/odeke-em/drive#initializing .
+gdrivedir="${HOME}/gdrive/Paper Drafts/"
+
 git config --local tex.main main
 git config --local tex.hash $hash
+git config --local tex.gdrivedir $gdrivedir
+
 for hook in "pre-commit" "post-commit"
 do
     cp ${repodir}/hooks/${hook} ${repodir}/.git/hooks/
